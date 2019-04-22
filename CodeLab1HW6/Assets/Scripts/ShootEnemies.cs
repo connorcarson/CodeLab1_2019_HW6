476cdd9efb61d118bd94a87c58cf6d2b6d781015
connorcarson <cc3237@nyu.edu>
2019-04-21 21:53:51 -0400

    Wrote functional ShootEnemies script - destroys enemy game objects utilizing raycast.

---
+++ CodeLab1HW6/Assets/Scripts/ShootEnemies.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemies : MonoBehaviour
+{
+    private Ray _myRay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        _myRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        //Debug.DrawRay(_myRay.origin, _myRay.direction * 1000f, Color.red);
+        
+        RaycastHit _myRaycastHit = new RaycastHit();
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(_myRay, out _myRaycastHit, 1000f))
+            {
+                if (_myRaycastHit.transform.gameObject == gameObject)
+                {
+                    Destroy(_myRaycastHit.transform.gameObject);
+                    Debug.Log("Hit!");
+                }
+                else
+                {
+                    Debug.Log("Miss!");
+                }
+            }
+        }
+    }
+}
