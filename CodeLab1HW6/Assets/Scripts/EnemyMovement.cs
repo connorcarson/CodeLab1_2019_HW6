ab410f188ee203773442e72df741d8518ea66392
connorcarson <cc3237@nyu.edu>
2019-04-21 22:12:36 -0400

    Wrote functional EnemyMovement script - enemy move freely around the level turning randomly either right or left when hitting a wall.

---
+++ CodeLab1HW6/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speedMultiplier;
+    public float maxRayDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        //move enemy forward always
+        transform.Translate(Vector3.forward * speedMultiplier * Time.deltaTime);
+        
+        //initialize ray shooting out from enemy
+        Ray enemyRay = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(enemyRay.origin, enemyRay.direction * maxRayDistance, Color.green);
+
+        if (Physics.Raycast(enemyRay, maxRayDistance))
+        {
+            int turnProbability = Random.Range(0, 100);
+            if (turnProbability <= 50)
+            {
+                transform.Rotate(0f, 90f, 0f);
+            }
+            else
+            {
+                transform.Rotate(0f, -90f, 0f);
+            }
+        }
+    }
+}
